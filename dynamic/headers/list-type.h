67d0f1b4b756ceda0f3c880ac1a7a48ba10c331a
Manoel Vilela <manoel_vilela@engineer.com>
2015-06-23 01:43:52 -0300

    More refactoring and add subclass member on list struct!

---
+++ dynamic/headers/list-type.h
@@ -0,0 +1,43 @@
+/* ================================================
+ *
+ *  -*- All structures definition are below  -*-
+ *
+ *=================================================
+ */
+
+
+// for aux the enums
+static const char *type_strings[] = { "integer", "character",  "real"};
+static const char *state_strings[] = {"empty", "available", "full"};
+
+typedef enum {
+    empty,
+    available,
+    full
+} id_state;
+
+
+typedef struct {
+    char subclass[10];
+    meta_data *elements;
+    id_state state;
+    int first_index;
+    int last_index;
+    int size;
+} list;
+
+
+/* ===================================================================
+ *
+ *                 -*-  A resume of a list struct    -*-
+ * const char *subclass := "queue" || "stack" || "deque"
+ *  meta_data *elements := {something data, identifier type}
+ *       id_state state := {0:empty, 1:available, 2:full}
+ *      int first_index := index of vector whose is used to control i/o
+ *       int last_index := index of vector whose is used to control i/o
+ *             int size := the lenght of queue
+ *            int state := {0:empty, 1:available, 2:full}
+ *
+ * ====================================================================
+ */
+
