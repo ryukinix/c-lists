4f1f1043d11f7705b6145508ad09180526f0b541
Manoel Vilela <manoel_vilela@engineer.com>
2015-06-25 06:46:19 -0300

    Adding new function to auxiliate the comparision of unions!

--- static/headers/meta-functions.h
+++ static/headers/meta-functions.h
@@ -5,6 +5,7 @@
  *===========================================================
  */
 
+#include <stdbool.h>
 
 void print_element(meta_data element) {
     identifier type = element.type;
@@ -36,3 +37,43 @@ something new_thing(identifier type){
 
     return the_thing;
 }
+
+int meta_comparision(meta_data x, meta_data y){
+    if (x.data.integer == y.data.integer)
+        return true;
+    else if (x.data.character == y.data.character)
+        return true;
+    else if (x.data.real == y.data.real)
+        return true;
+
+    return false;
+}
+
+/* // conflicts with the definition in list-type
+void type_choose(meta_data *element){
+    int command;  
+    
+    puts("Insert the type that you wish attribute!");
+    puts("1.Int\n2.Char\n3.Real\n");
+
+    printf("Put a command: ");
+    scanf("%d", &command);
+    clear_buffer();
+
+    switch(command){
+        case 1:
+            element->type = integer;
+            break;
+        case 2:
+            element->type = character;
+            break;
+        case 3:
+            element->type = real;
+            break;
+        default:
+            puts("Invalid option! Try again!");
+            type_choose(element);
+            break;       
+    }  
+}
+*/
