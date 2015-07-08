ebf2878cf95e745b5320b142051f8d4095f290f7
Manoel Vilela <manoel_vilela@engineer.com>
2015-07-07 21:10:59 -0300

    Need fix yet | basic linked list funcs

--- dynamic/headers/list-functions.h
+++ dynamic/headers/list-functions.h
@@ -1,153 +1,122 @@
-/* =============================================
+/* ================================================
  *
  *  -*- Definition of all basic functions list  -*-
+ *
+ * ================================================
+ */
+
+
+#include <time.h>
+
+
+/* =============================================
+ *
+ *  -*-   Node control for linked list      -*-
  *
  * =============================================
  */
 
-#include <time.h>
+// walk to a next node
+node* walk_foward(node *n){
+    n = n->next
+    return n;
+}
+node* walk_foward(node *n){
+    n = n->back
+    return n;
+}
 
-//print the list
+// walk until the n node of the linked list
+node* walk_until(node *head, int index){
+    int i;
+    node *n = head;
+    for (i = 0; i < index && n != NULL; i++)
+        n = walk_foward(n);
+    return n;
+}
+
+node* walk_to_tail(node *head){
+    int i;
+    node *n = head;
+    for (i = 0; i < index && n != NULL; i++)
+        n = walk_foward(n);
+    return n;
+}
+
+// print the list
 void print_list(list *l) {
     int i;
-    identifier type = l->elements[0].type;
+    identifier type = l->elements.type;
+    node *n = l->elements;
 
     printf("%s: [ ", l->subclass);
-    for (i = 0; i < l->size; i++)
-        print_element(l->elements[i]);
+    for (i = 0; l->elements != NULL; i++){
+        print_element(n->meta);
+        n = walk_foward(n);
+    }
     printf("]\n");
 }
 
 
-//insert on index
+// insert on index
 void insert_on(list *l, int index) {
-    something the_thing = new_thing(l->elements[0].type);
-    l->elements[index].data = the_thing;
-}
-
-
-// rearrange the list
-void rearrange(list *l) {
-    // ignoring empty blocks and rearrange the vector
-    int i, j;
-    for (i = 0, j = 0; i < l->size; i++) {
-        if (l->elements[i].data.integer != -1) {
-            l->elements[j] = l->elements[i];
-            j++;
-        }
-    }
-
-    // assigning the garbage in right of vector as empty
-    for (i = j; i < l->size; i++) {
-        l->elements[i].data.integer = -1;
-    }
-
-    l->last_index = j - 1;
+    something the_thing = new_thing(l->elements->type);
+    walk_until(l, index);
+    l->elements.n.data = the_thing;
 }
 
 
 // delete some element
-void del(list *l, int index) {
-    l->elements[index].data.integer = -1;
-    rearrange(l);
+void del(list *l, int index){
+    walk_until(l, index);
+    free(l->elements);
 }
 
-//clear the list
+// clear the list
 void clear_list(list *l){
-    int index;
-    for(index = 0; index < l->size; index++)
-        l->elements[index].data.integer = -1;
-    rearrange(l);
+    l->size = 0;
+    l->elements = NULL;
+    l->id_state = empty;
 }
 
-//verify the state of list
+// verify the state of list
 void verify_state(list *l) {
-    if (l->last_index == -1)
+    if (size == 0)
         l->state = empty;
-    else if (l->last_index >= l->size - 1)
-        l->state = full;
     else
         l->state = available;
 }
 
-//choose the type of the list
-void type_choose(list *l) {
-    int command, i;
-    puts("Choose a type for the list");
-    printf("1.Int\n2.Char\n3.Float\n\n");
-    printf("Enter a command: ");
-    scanf("%d", &command);
-    clear_buffer();
+// start the list 
+void start(list *l) {
+    // initial size;
+    l->size = 0;
 
-    for (i= 0; i < l->size; i++) {
-        //that verification it will usefull on the type change
-        if (l->elements[i].data.integer == -1){
-            if (command == 1)
-               l->elements[i].type = integer;
-            else if (command == 2)
-               l->elements[i].type = character;
-            else if (command == 3)
-               l->elements[i].type = real;
-            else        
-               type_choose(l);
-        }
-    }
-}
-
-
-/* ===============================================
- *
- *             -*- Meta comment -*-
- *
- * Was adopted that the begin index to control is -1,
- * Like this as the empty value too.
- * 
- * ===============================================
- */
-
-
-void start(list *q) {
-    // get the size of list
-    int size = generate("Enter a size: ");
-    q->size = size;
-
-    // alloc memory
-    q->elements = (meta_data *) malloc(sizeof(meta_data) * size);
-    if (q->elements == NULL) {
-        puts("Error with alloc memory! Try again with other size.");
-        start(q);
-    }
+    // point to null
+    l->elements = NULL; 
 
     // start the initial values
-    q->last_index = -1;
-    q->state = empty;
-
-    int i;
-    for (i = 0; i < q->size; i++) {
-        q->elements[i].data.integer = -1;
-    }
-
-    //Ask the type to receive
-    type_choose(q);
+    l->state = empty;
 }
 
 
-//insert random values all on the list
+// insert random values all on the list
 void random_values(list *l) {
     int i;
     time_t t;
     srand((unsigned) time(&t));
 
+    node *n = l->elements;
+
     for (i = 0; i < l->size; i++) {
-        identifier type = l->elements[i].type;
+        identifier type = l->elements.meta.type;
         if (type == integer)
-            l->elements[i].data.integer = rand() % 100;
+            n->meta.data.integer = rand() % 100;
         else if (type == character)
-            l->elements[i].data.character = 'a' + rand() % 26;
+            n->meta.data.character = 'a' + rand() % 26;
         else if (type == real)
-            l->elements[i].data.real = (rand() % 100 / ( (rand () % 10) + 1));
+            n->data.real = (rand() % 100 / ( (rand () % 10) + 1));
+        n = walk_foward(n);
     }
-    
-    l->last_index = i - 1;
 }
 
