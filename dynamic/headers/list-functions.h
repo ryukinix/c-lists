84a692df2bb8db35f038d4f85c25bd5fd27bee92
Rafael C. Nunes <rafaelnunes737@hotmail.com>
2015-07-14 10:19:32 -0300

    I think that I solved the error, check it out.

--- dynamic/headers/list-functions.h
+++ dynamic/headers/list-functions.h
@@ -16,13 +16,11 @@
  * =============================================
  */
 
-// walk to a next node
-node* walk_foward(node *n){
+node* walk_forward(node *n){
     n = n->next;
     return n;
 }
 
-// don't you mean walk backwards?
 node* walk_backwards(node *n){
     n = n->back;
     return n;
@@ -44,7 +42,7 @@ node* walk_until(node *head, int index){
     int i;
     node *n = head;
     for (i = 0; i < index && n != NULL; i++)
-        n = walk_foward(n);
+        n = walk_forward(n);
     return n;
 }
 
@@ -53,14 +51,16 @@ node* walk_to_tail(node *head){
 
     int i;
     for (i = 0; n != NULL; i++)
-        n = walk_foward(n);
+        n = walk_forward(n);
     return n;
 }
 
 void remove_node(node **some_node){
-    free(some_node);
-    // maybe here's the error.
-    some_node = (*some_node)->next;
+    // erasing the reference to the pointer?
+    //free(some_node);
+
+    free(*some_node);
+    *some_node = (*some_node)->next;
 }
 
 // END
@@ -73,7 +73,7 @@ void print_list(list *l) {
     printf("%s: [ ", l->subclass);
     for (i = 0; l->elements != NULL; i++){
         print_element(n->meta);
-        n = walk_foward(n);
+        n = walk_forward(n);
     }
     printf("]\n");
 }
@@ -122,7 +122,7 @@ void random_values(list *l) {
             n->meta.data.character = 'a' + rand() % 26;
         else if (type == real)
             n->meta.data.real = (rand() % 100 / ( (rand () % 10) + 1));
-        n = walk_foward(n);
+        n = walk_forward(n);
     }
 }
 
